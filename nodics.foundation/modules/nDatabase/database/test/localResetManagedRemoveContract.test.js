075f40a16b5901c6533a11f1f8b25dfcfb039516
Himkar Dwivedi <himkar.dwivedi@gmail.com>
2026-09-11 21:29:19 +0400

    chore: align qualification sources with copyright gate

--- nodics.foundation/modules/nDatabase/database/test/localResetManagedRemoveContract.test.js
+++ nodics.foundation/modules/nDatabase/database/test/localResetManagedRemoveContract.test.js
@@ -1,4 +1,14 @@
-/* Copyright (c) 2026 Nodics. Governed by the root LICENSE. */
+/*
+    Nodics - Enterprice Micro-Services Management Framework
+
+    Copyright (c) 2026 Nodics All rights reserved.
+
+    This software is governed by the Nodics Source-Available Commercial License.
+    You may use, copy, modify, deploy, or distribute it only as permitted by the
+    root LICENSE file or a separate written agreement with Nodics.
+
+ */
+
 "use strict";
 
 /** @module database/test/localResetManagedRemoveContract @description Verifies opaque Local reset authority with managed schemas while rejecting forged bulk-delete requests. @layer test @owner database */
