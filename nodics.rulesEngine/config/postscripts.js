bbdf312ceb4258dd92bc9ac95cb68f951d40fc47
Himkar <himkar.dwivedi@gmail.com>
2026-09-20 12:58:43 +0400

    feat(rules): establish functional module foundation

---
+++ nodics.rulesEngine/config/postscripts.js
@@ -0,0 +1,13 @@
+/*
+    Nodics - Enterprice Micro-Services Management Framework
+
+    Copyright (c) 2026 Nodics All rights reserved.
+
+    This software is governed by the Nodics Source-Available Commercial License.
+    You may use, copy, modify, deploy, or distribute it only as permitted by the
+    root LICENSE file or a separate written agreement with Nodics.
+
+ */
+
+/** @module nodics.rulesEngine/config/postscripts @description Rules Engine group post-start extension declarations. @layer config @owner nodics.rulesEngine */
+module.exports = {};
