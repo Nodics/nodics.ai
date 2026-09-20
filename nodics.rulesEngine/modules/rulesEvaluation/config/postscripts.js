542a36b005152cbfd729371d86f1864f96c36896
Himkar <himkar.dwivedi@gmail.com>
2026-09-20 13:01:43 +0400

    feat(rules): complete rulesEvaluation contract surface

---
+++ nodics.rulesEngine/modules/rulesEvaluation/config/postscripts.js
@@ -0,0 +1,13 @@
+/*
+    Nodics - Enterprice Micro-Services Management Framework
+
+    Copyright (c) 2026 Nodics All rights reserved.
+
+    This software is governed by the Nodics Source-Available Commercial License.
+    You may use, copy, modify, deploy, or distribute it only as permitted by the
+    root LICENSE file or a separate written agreement with Nodics.
+
+ */
+
+/** @module rulesEvaluation/config/postscripts @description Post-start extension declarations for rulesEvaluation. @layer config @owner rulesEvaluation */
+module.exports = {};
