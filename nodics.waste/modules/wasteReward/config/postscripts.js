859716a2b354a1c3b24c0357ead550a995566cdd
Himkar <himkar.dwivedi@gmail.com>
2026-09-20 14:16:40 +0400

    feat(waste): add reusable reward assessment capability

---
+++ nodics.waste/modules/wasteReward/config/postscripts.js
@@ -0,0 +1,13 @@
+/*
+    Nodics - Enterprice Micro-Services Management Framework
+
+    Copyright (c) 2026 Nodics All rights reserved.
+
+    This software is governed by the Nodics Source-Available Commercial License.
+    You may use, copy, modify, deploy, or distribute it only as permitted by the
+    root LICENSE file or a separate written agreement with Nodics.
+
+ */
+
+/** @module wasteReward/config/postscripts @description Post-start extension declarations for wasteReward. @layer config @owner wasteReward */
+module.exports = {};
