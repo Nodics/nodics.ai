fca7bd643f290608d39a48857cc65b29e61645b1
Himkar <himkar.dwivedi@gmail.com>
2026-09-20 13:01:56 +0400

    feat(rules): complete rulesEvaluation contract surface

---
+++ nodics.rulesEngine/modules/rulesEvaluation/src/utils/utils.js
@@ -0,0 +1,13 @@
+/*
+    Nodics - Enterprice Micro-Services Management Framework
+
+    Copyright (c) 2026 Nodics All rights reserved.
+
+    This software is governed by the Nodics Source-Available Commercial License.
+    You may use, copy, modify, deploy, or distribute it only as permitted by the
+    root LICENSE file or a separate written agreement with Nodics.
+
+ */
+
+/** @module rulesEvaluation/src/utils/utils @description Generic evaluation utility extension surface. @layer utility @owner rulesEvaluation */
+module.exports = {};
